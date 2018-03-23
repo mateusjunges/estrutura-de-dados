ca9398f331c41c5fc12100ddda58f914180775f3
Mateus <mateusf.junges@gmail.com>
2018-03-22 22:42:22 -0300

    tabela hash

---
+++ tabelahash.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+
+const int TAM = 13;
+
+int hash1[TAM];
+int tamHash1 = 0;
+int hash2[TAM];
+int tamHash2 = 0;
+int chave;
+
+void preencheVetor(int v[])
+{
+    for(int i=0; i < TAM; i++)
+        v[i]=-1;
+}
+
+void imprimeVetor(int v[])
+{
+    for(int i=0; i < TAM; i++)
+        cout << v[i] << endl;
+}
+int h(int chave)
+{
+    return chave%TAM;
+}
+
+int rh1(int posAtual)
+{
+    int pos = (posAtual + 1)%TAM;
+    return pos;
+}
+
+int rh2(int posAtual)
+{
+    int aux = 1+ h(chave)%(TAM-1);
+    int pos = (posAtual + aux)%TAM;
+    return pos;
+}
+
+
+void insere(int chave, int vetorHash[], int tamHash, int (*reHash)(int))
+{
+    int pos  = h(chave);
+    if(tamHash == TAM)
+    {
+        cout << "Hash cheia " << endl;
+        return;
+    }
+    tamHash++;
+    if(vetorHash[pos]==-1)
+        vetorHash[pos]=chave;
+    else
+    {
+        do
+        {
+            pos =(*reHash)(h(chave));
+        }
+        while(vetorHash[pos] != -1);
+        vetorHash[pos]=chave;
+    }
+    cout << "Inseriu" << endl;
+}
+
+void buscavoid(int chave, int vetorHash[], int tamHash, int (*reHash)(int))
+{
+    int posAtual= h(chave);
+    int numBuscas;
+    if(vetorHash[posAtual]==-1)
+    {
+        cout << "Não achou" << endl;
+        return;
+    }
+    while(numBuscas == TAM || vetorHash[posAtual] == -1)
+    {
+        if(vetorHash[posAtual]==chave)
+        {
+            cout << "Achou" << endl;
+            return;
+        }
+        else
+        {
+            posAtual =(*reHash)(posAtual);
+        }
+        numBuscas++;
+    }
+    if(numBuscas == TAM || vetorHash[posAtual] == -1)
+        cout << "Elemento não encontrado" << endl;
+}
